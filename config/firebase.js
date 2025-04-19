ddc49dbe318578e3280b078dd8edbf563ae62bad
ThongHa <haminhthong0811@gmail.com>
2025-04-19 13:00:46 +0700

    init

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+// import firebaseAdmin from "firebase-admin";
+// import serviceAccount from "";
+
+// firebaseAdmin.initializeApp({
+//   credential: firebaseAdmin.credential.cert(serviceAccount),
+//   storageBucket: "",
+// });
+
+// export const bucket = firebaseAdmin.storage().bucket();
